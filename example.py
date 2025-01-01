cd193437d9c32c7c4ceb8e9df883a92171e717ed
Altaseb-chernet <altacher227@gmail.com>
2025-01-01 06:39:17 -0800

    turtle example

---
+++ example.py
@@ -0,0 +1,34 @@
+import turtle  
+
+alta = turtle.Turtle()
+alta.screen.bgcolor('blue')
+alta.screen.title('turtle shapes')
+alta.color('gold')
+alta.begin_fill()
+alta.pencolor('green')
+alta.pensize(10)
+alta.forward(100)
+alta.left(90)
+alta.forward(100)
+alta.left(90)
+alta.forward(100)
+alta.left(90)
+alta.forward(100)
+alta.left(90)
+
+alta.penup()
+alta.forward(150)
+alta.pendown()
+
+alta.forward(100)
+alta.left(90)
+alta.forward(100)
+alta.left(90)
+alta.forward(100)
+alta.left(90)
+alta.forward(100)
+alta.left(90)
+
+
+alta.end_fill()
+turtle.done()
